c6cd241bcf1ecf1ba62732517e35a92949e0fbe8
bcheung <brandon.cheung.dev@gmail.com>
2024-04-25 13:19:57 -0400

    add reference comment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  // https://stackoverflow.com/questions/70639657/tailwind-css-not-working-properly-in-nextjs
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
