235e2d223be1b7b5cf60c67f4e2b5cbae9838785
bcheung <brandon.cheung.dev@gmail.com>
2024-04-25 23:12:24 -0400

    add static category and tag data seeding

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -6,3 +6,5 @@ set -eu
 npx tsx ./prisma/seeds/20240424105830-add-home-icon.ts
 
 npx tsx ./prisma/seeds/20240424185825-add-icons.ts
+
+npx tsx ./prisma/seeds/20240425225318-add-static-categories.ts
