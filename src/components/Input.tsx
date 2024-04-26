d625c47df9e8a4315735985c7850d29f927e3bb1
bcheung <brandon.cheung.dev@gmail.com>
2024-04-25 20:54:12 -0400

    add input and twmerge

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,20 +1,39 @@
+import Image from "next/image";
 import React from "react";
+import { ClassNameValue, twMerge } from "tailwind-merge";
 
 interface InputProps {
   label: string;
   name?: string;
   type?: React.HTMLInputTypeAttribute;
+  placeholder?: string;
+  step?: number;
+  inputClassName?: ClassNameValue;
 }
 
-export default function Input({ label, name, type }: InputProps) {
+export default function Input({
+  label,
+  name,
+  type,
+  placeholder,
+  inputClassName,
+  step,
+}: InputProps) {
   return (
-    <div className="flex flex-col gap-2">
-      <label className="font-semibold text-heading3">{label}</label>
+    <div className="flex flex-col gap-2 relative">
+      <label htmlFor={name ?? label} className="font-semibold text-heading3">
+        {label}
+      </label>
       <input
+        id={name ?? label}
         type={type}
-        placeholder={label}
-        className="outline-none border-none p-4 rounded-2xl bg-secondary shadow-md"
+        placeholder={placeholder ?? label}
+        className={twMerge(
+          "outline-none border-none p-4 rounded-2xl bg-secondary shadow-md",
+          inputClassName
+        )}
         name={name ?? label}
+        step={step}
       />
     </div>
   );
