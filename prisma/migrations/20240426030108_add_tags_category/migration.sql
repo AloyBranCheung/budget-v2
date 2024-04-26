e2f2f939a69fce3a42b7dfa0b2ef9003cadeb567
bcheung <brandon.cheung.dev@gmail.com>
2024-04-25 23:09:18 -0400

    add tags table

---
+++ prisma/migrations/20240426030108_add_tags_category/migration.sql
@@ -0,0 +1,31 @@
+-- AlterTable
+ALTER TABLE "Category" ADD COLUMN     "description" TEXT;
+
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_TagToTransaction" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_TagToTransaction_AB_unique" ON "_TagToTransaction"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_TagToTransaction_B_index" ON "_TagToTransaction"("B");
+
+-- AddForeignKey
+ALTER TABLE "_TagToTransaction" ADD CONSTRAINT "_TagToTransaction_A_fkey" FOREIGN KEY ("A") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_TagToTransaction" ADD CONSTRAINT "_TagToTransaction_B_fkey" FOREIGN KEY ("B") REFERENCES "Transaction"("id") ON DELETE CASCADE ON UPDATE CASCADE;
