f52314dcc4ca045c9b6e9d67d0059b9f95aceae1
bcheung <brandon.cheung.dev@gmail.com>
2024-04-29 09:55:11 -0400

    add tmp logout button

--- src/app/app/profile/page.tsx
+++ src/app/app/profile/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+import { signOut } from "next-auth/react";
 import React from "react";
 
 export default function ProfilePage() {
-  return <div>add back link here, ProfilePage</div>;
+  return (
+    <div className="flex items-center justify-center flex-col">
+      <p>add back link here, ProfilePage</p>
+      <button
+        onClick={() =>
+          signOut({ callbackUrl: process.env.NEXT_PUBLIC_BASE_URL })
+        }
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
