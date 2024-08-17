394f66f293cf881f147f4c4c5cf08216c82a3ce2
bcheung <brandon.cheung.dev@gmail.com>
2024-08-17 09:53:43 -0400

    fix tests

--- src/__tests__/unit/overviewcard.test.tsx
+++ src/__tests__/unit/overviewcard.test.tsx
@@ -26,8 +26,6 @@ describe("test OverviewCard component", () => {
       />
     );
 
-    screen.debug();
-
     expect(screen.queryByText("$ 1,000.00")).not.toBeNull();
     expect(screen.queryByText("Since Tuesday, April 23, 2024")).not.toBeNull();
   });
