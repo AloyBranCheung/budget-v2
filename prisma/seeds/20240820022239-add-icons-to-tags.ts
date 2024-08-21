4a46be189d5d26bac3c440896a819494993dfa7f
brandon <brandon.cheung.dev@gmail.com>
2024-08-21 08:28:51 -0400

    wip: get-transactions-filtered server action int. test, optimize seed

--- prisma/seeds/20240820022239-add-icons-to-tags.ts
+++ prisma/seeds/20240820022239-add-icons-to-tags.ts
@@ -1,15 +1,17 @@
 import addIconToDb from '../utils/add-icon-to-db';
 
 const main = async () => {
-    await addIconToDb('tag-home-icon.png', { connectToTag: true, tagName: 'Housing' })
-    await addIconToDb('transportation-icon.png', { connectToTag: true, tagName: 'Transportation' })
-    await addIconToDb('utilities-icon.png', { connectToTag: true, tagName: 'Utilities' })
-    await addIconToDb('first-aid-icon.png', { connectToTag: true, tagName: 'Medical & Healthcare' })
-    await addIconToDb('savings-icon.png', { connectToTag: true, tagName: 'Savings' })
-    await addIconToDb('cutlery-icon.png', { connectToTag: true, tagName: 'Food' })
-    await addIconToDb('budget-icon.png', { connectToTag: true, tagName: 'Personal Spendings' })
-    await addIconToDb('entertainment-icon.png', { connectToTag: true, tagName: 'Recreation & Entertainment' })
-    await addIconToDb('categorize-icon.png', { connectToTag: true, tagName: 'Miscellaneous' })
+    await Promise.all([
+        addIconToDb('tag-home-icon.png', { connectToTag: true, tagName: 'Housing' }),
+        addIconToDb('transportation-icon.png', { connectToTag: true, tagName: 'Transportation' }),
+        addIconToDb('utilities-icon.png', { connectToTag: true, tagName: 'Utilities' }),
+        addIconToDb('first-aid-icon.png', { connectToTag: true, tagName: 'Medical & Healthcare' }),
+        addIconToDb('savings-icon.png', { connectToTag: true, tagName: 'Savings' }),
+        addIconToDb('cutlery-icon.png', { connectToTag: true, tagName: 'Food' }),
+        addIconToDb('budget-icon.png', { connectToTag: true, tagName: 'Personal Spendings' }),
+        addIconToDb('entertainment-icon.png', { connectToTag: true, tagName: 'Recreation & Entertainment' }),
+        addIconToDb('categorize-icon.png', { connectToTag: true, tagName: 'Miscellaneous' })
+    ])
 }
 
 main()
--- src/__tests__/integration/get-transactions-filtered.test.ts
+++ src/__tests__/integration/get-transactions-filtered.test.ts
@@ -1,5 +1,76 @@
+import { describe, it, beforeEach, vi, afterEach } from 'vitest';
+import { render, screen, cleanup } from '@testing-library/react'
+import prisma from '@/libs/prisma';
+import { TransactionType } from '@prisma/client';
 // test this
 import getTransactionsFiltered from "@/actions/get-transactions-filtered";
 
 // TODO: write integration test for filter transaction server action
 // TODO: test results are filtered by date range, transaction type, tags and a combination of them 
+
+
+describe('test get-transactions-filtered server action/api thing', () => {
+    beforeEach(async () => {
+        vi.useFakeTimers();
+        const date = new Date('2024-08-01')
+        vi.setSystemTime(date);
+
+        // todo: add mock transactions
+        const user = await prisma.user.findFirst({
+            where: {
+                email: 'test@test.com'
+            }
+        })
+        if (!user) throw new Error("Test user not found.")
+
+        const category = await prisma.category.findFirst({
+            where: {
+                name: 'Needs',
+            }
+        })
+        if (!category) throw new Error("Test category not found.")
+
+        const paycheck = await prisma.paycheck.create({
+            data: {
+                amount: 1000,
+                userId: user.id
+            }
+        })
+
+        await prisma.transaction.createMany({
+            data: [
+                {
+                    amount: 100.69,
+                    name: 'test expense',
+                    type: TransactionType.Expense,
+                    date,
+                    notes: 'test expense note',
+                    userId: user.id,
+                    categoryId: category.id,
+                    paycheckId: paycheck.id,
+                },
+                {
+                    amount: 100.69,
+                    name: 'test income',
+                    type: TransactionType.Income,
+                    date,
+                    notes: 'test income note',
+                    userId: user.id,
+                    categoryId: category.id,
+                    paycheckId: paycheck.id,
+                }
+            ]
+        })
+    })
+
+    afterEach(async () => {
+        vi.useRealTimers()
+        cleanup()
+    })
+
+    it('should return last 30 days', async () => {
+        const transactions = await prisma.transaction.findMany();
+
+        console.log(transactions);
+    })
+})
