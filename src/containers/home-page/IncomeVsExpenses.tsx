0718397c6269b0dcc0f945489ca7521eca6a30c9
bcheung <brandon.cheung.dev@gmail.com>
2024-08-25 00:09:43 -0400

    style chart

--- src/containers/home-page/IncomeVsExpenses.tsx
+++ src/containers/home-page/IncomeVsExpenses.tsx
@@ -3,6 +3,7 @@
 import Card from '@/components/Card';
 import React from 'react'
 import { ReferenceLine, Bar, BarChart, LabelList, Legend, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import { capitalize } from 'lodash'
 // type 
 import { ChartData } from '@/actions/get-incomevexpenses';
 
@@ -20,16 +21,16 @@ export default function IncomeVsExpenses({ data }: IncomeVsExpensesProps) {
             <h4>Expenses Per Month</h4>
             <Card className='h-96 w-full overflow-x-auto'>
                 <ResponsiveContainer width="100%" height="100%">
-                    <BarChart stackOffset='sign' height={384} data={data}>
+                    <BarChart stackOffset='sign' height={384} data={data} barSize={15}>
                         <YAxis hide padding={{ bottom: 25 }} />
-                        <XAxis dataKey='name' />
-                        <Legend />
+                        <XAxis dataKey='name' tick={{ fill: 'rgb(0,0,0)' }} stroke="#000" strokeWidth={2} />
+                        <Legend formatter={(value) => <p className='text-black'>{capitalize(value)}</p>} />
                         <Bar dataKey="income" stackId="a" fill="#A2D9A8" radius={[16, 16, 0, 0]}>
-                            <LabelList dataKey="income" position="top" formatter={formatLabel} />
+                            <LabelList dataKey="income" position="top" formatter={formatLabel} fill="#000" />
                         </Bar>
-                        <ReferenceLine y={0} />
+                        <ReferenceLine y={0} strokeWidth={1} />
                         <Bar dataKey="expense" stackId="a" fill="#F3D0D7" radius={[16, 16, 0, 0]}>
-                            <LabelList dataKey="expense" position="top" formatter={formatLabel} />
+                            <LabelList dataKey="expense" position="top" formatter={formatLabel} fill="#000" />
                         </Bar>
                     </BarChart>
                 </ResponsiveContainer>
