5cef3a1617373cfd97bccc55f23300bcaaee41cb
bcheung <brandon.cheung.dev@gmail.com>
2024-08-25 09:08:07 -0400

    change chart style... again

--- src/containers/home-page/IncomeVsExpenses.tsx
+++ src/containers/home-page/IncomeVsExpenses.tsx
@@ -23,14 +23,14 @@ export default function IncomeVsExpenses({ data }: IncomeVsExpensesProps) {
                 <ResponsiveContainer width="100%" height="100%">
                     <BarChart stackOffset='sign' height={384} data={data} barSize={15}>
                         <YAxis hide padding={{ bottom: 25 }} />
-                        <XAxis dataKey='name' tick={{ fill: 'rgb(0,0,0)' }} stroke="#000" strokeWidth={2} />
-                        <Legend formatter={(value) => <p className='text-black'>{capitalize(value)}</p>} />
+                        <XAxis dataKey='name' strokeWidth={2} />
+                        <Legend formatter={(value) => <p >{capitalize(value)}</p>} />
                         <Bar dataKey="income" stackId="a" fill="#A2D9A8" radius={[16, 16, 0, 0]}>
-                            <LabelList dataKey="income" position="top" formatter={formatLabel} fill="#000" />
+                            <LabelList dataKey="income" position="top" formatter={formatLabel} />
                         </Bar>
                         <ReferenceLine y={0} strokeWidth={1} />
                         <Bar dataKey="expense" stackId="a" fill="#F3D0D7" radius={[16, 16, 0, 0]}>
-                            <LabelList dataKey="expense" position="top" formatter={formatLabel} fill="#000" />
+                            <LabelList dataKey="expense" position="top" formatter={formatLabel} />
                         </Bar>
                     </BarChart>
                 </ResponsiveContainer>
