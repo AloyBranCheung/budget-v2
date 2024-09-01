24218e9af9eab919c4288c7213d09332c87fe6dd
brandon <brandon.cheung.dev@gmail.com>
2024-09-01 19:11:07 -0400

    add auth middleware

--- src/app/api/transactions/income-v-expenses/route.ts
+++ src/app/api/transactions/income-v-expenses/route.ts
@@ -2,6 +2,5 @@ import getUser from "@/auth/get-user";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-    const user = getUser();
-    if (!user) return new NextResponse("Unauthorized", { status: 401 });
+    return new NextResponse('test', {status: 200})
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,3 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { getServerSession } from "next-auth";
-// This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
-  const session = await getServerSession();
-  if (!session) return NextResponse.redirect("/");
+export { default } from "next-auth/middleware"
 
-  console.log("middleware hit");
-  return NextResponse.next();
-}
-
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: "/api/transactions/:path*",
-};
+export const config = { matcher: ["/api/transactions/:path*"] }
