e3cc9c8044116fd67b225b164830ebc7e9703816
bcheung <brandon.cheung.dev@gmail.com>
2024-09-02 08:42:43 -0400

    add icons to tag

--- prisma/seeds/20240902043422-add-income-tags.ts
+++ prisma/seeds/20240902043422-add-income-tags.ts
@@ -1,5 +1,7 @@
-import { PrismaClient, Prisma, TransactionType } from "@prisma/client";
+import { PrismaClient, TransactionType } from "@prisma/client";
 import pino from "pino";
+// utils
+import addIconToDb from "../utils/add-icon-to-db";
 
 const logger = pino({
   transport: {
@@ -13,27 +15,41 @@ const logger = pino({
 const prisma = new PrismaClient();
 
 const main = async () => {
-  const incomeTags: Prisma.TagCreateManyInput[] = [
+  const incomeTags: {
+    name: string;
+    type: TransactionType;
+    filename: string;
+  }[] = [
     // TODO: add default descriptions
     {
       name: "Allowance",
       type: TransactionType.Income,
+      filename: "allowance-icon.png",
     },
     {
       name: "Salary",
       type: TransactionType.Income,
+      filename: "salary-icon.png",
     },
     {
       name: "Petty Cash",
       type: TransactionType.Income,
+      filename: "petty-cash-icon.png",
     },
     {
       name: "Bonus",
       type: TransactionType.Income,
+      filename: "bonus-icon.png",
     },
     {
       name: "Others",
       type: TransactionType.Income,
+      filename: "more-icon.png",
+    },
+    {
+      name: "e-transfer",
+      type: TransactionType.Income,
+      filename: "e-transfer-icon.png",
     },
   ];
 
@@ -50,10 +66,17 @@ const main = async () => {
 
   // add tags if it does not exist
   await prisma.tag.createMany({
-    data: incomeTags,
+    data: incomeTags.map(({ name, type }) => ({ name, type })),
   });
 
   logger.info(`Added ${incomeTags.length} income tags.`);
+
+  // add icon and connect tag
+  await Promise.all(
+    incomeTags.map(({ filename, name }) =>
+      addIconToDb(filename, { connectToTag: true, tagName: name }),
+    ),
+  );
 };
 
 main();
