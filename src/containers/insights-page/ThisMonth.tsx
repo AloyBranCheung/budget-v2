df63b3c9b71e5bbf44dc3adb6acf039d2f86b2ab
bcheung <brandon.cheung.dev@gmail.com>
2024-09-11 22:27:39 -0400

    wip: this month

--- src/containers/insights-page/ThisMonth.tsx
+++ src/containers/insights-page/ThisMonth.tsx
@@ -2,8 +2,19 @@ import React from "react";
 
 export default function ThisMonth() {
   return (
-    <div>
-      <h4>This Month</h4>
+    <div className="flex flex-col gap-2">
+      <div className="flex items-center justify-between">
+        <h4>This Month</h4>
+        <p>More Details</p>
+      </div>
+      <div>
+        Income vs Expense Chart for This Month (or more than just this month?)
+      </div>
+      <div>Piechart for each eategory broken down by tags in each category</div>
+      <div>
+        brush chart (both ways including income) for total spent this month in
+        tag ordered by highest expenditure first then income last
+      </div>
     </div>
   );
 }
